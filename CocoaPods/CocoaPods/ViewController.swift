e0419762f901a95079bdab385935cb43460d9bdb
PaulVagner <prodigyzro@gmail.com>
2015-10-13 17:38:10 -0400

    updated

---
+++ CocoaPods/CocoaPods/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  CocoaPods
+//
+//  Created by Paul Vagner on 10/12/15.
+//  Copyright © 2015 Paul Vagner. All rights reserved.
+//
+
+import UIKit
+import AFNetworking
+import SwiftyJSON
+
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
